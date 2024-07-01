0553d95fef939d07bdda08eb6c4662550c8b3a2f
loks0n <22452787+loks0n@users.noreply.github.com>
2024-07-01 11:12:37 +0100

    chore: revert labels

--- src/Orchestration/Adapter/DockerAPI.php
+++ src/Orchestration/Adapter/DockerAPI.php
@@ -207,10 +207,9 @@ class DockerAPI extends Adapter
 
         if ($result['code'] === 404) {
             throw new Orchestration('Network with name "'.$name.'" does not exist: '.$result['response']);
-        } elseif ($result['code'] !== 204) {
+        } else if ($result['code'] !== 204) {
             throw new Orchestration('Error removing network: '.$result['response']);
         }
-
         return true;
     }
 
@@ -472,6 +471,9 @@ class DockerAPI extends Adapter
         }
 
         $vars = $parsedVariables;
+
+        $labels[$this->namespace.'-type'] = 'runtime';
+        $labels[$this->namespace.'-created'] = (string) time();
 
         $body = [
             'Hostname' => $hostname,
